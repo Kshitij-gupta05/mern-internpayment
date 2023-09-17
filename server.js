89c3ef4f23f733f0739c232d10e16e225bec2fc9
kshitij_gupta05 <kshitij.gupta6350@gmail.com>
2023-09-17 09:00:26 +0530

    payment by kshitij

---
+++ server.js
@@ -0,0 +1,50 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+const cors = require("cors")
+app.use(express.json())
+
+const BASE_URL=process.env.BASE_URL;
+
+const corsorgin={
+    origin:`${BASE_URL}`
+};
+app.use(
+    cors(corsorgin)
+)
+
+const key= process.env.STRIPE_KEY;
+const stripe = require("stripe")(key);
+app.post("/create-checkout-session", async (req, res)=>{
+    try{
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types:["card"],
+            mode:"payment",
+            line_items: req.body.items.map(item => {        // josn given to stripe
+                return{
+                    price_data:{
+                        currency:"inr",
+                        product_data:{
+                            name: item.name
+                        },
+                        unit_amount: (item.price)*100,
+
+                    },
+                    quantity: item.quantity
+                }
+            }),
+            success_url: `${BASE_URL}/success`,
+            cancel_url: `${BASE_URL}/cancel`
+        })
+
+        res.json({url: session.url})
+
+    }catch(e){
+     res.status(500).json({error:e.message})
+    }
+})
+
+app.listen(3001,()=>{
+    console.log("server is running");
+})
